28ebef93a7a0ac0629fd24a831450538e7037223
Gareth Spring <gareth.spring@gmail.com>
2019-12-08 16:37:44 +0000

    Tidy

--- advent-of-code-2019/Day07/Cpu.cs
+++ advent-of-code-2019/Day07/Cpu.cs
@@ -53,7 +53,6 @@ namespace advent_of_code_2019.Day07
 
                     opCode = Convert.ToInt32(stringCode.Substring(3, 2));
 
-
                     mode1 = Convert.ToInt32(stringCode.Substring(2, 1));
                     mode2 = Convert.ToInt32(stringCode.Substring(1, 1));
                     mode3 = Convert.ToInt32(stringCode.Substring(0, 1));
@@ -67,35 +66,28 @@ namespace advent_of_code_2019.Day07
                 switch (opCode)
                 {
                     case 1:
-                        OpCode1(opPointer + 1, opPointer + 2, opPointer + 3, mode1, mode2, mode3);
-                        opPointer += 4;
+                        Add(opPointer + 1, opPointer + 2, opPointer + 3, mode1, mode2, mode3);
                         break;
                     case 2:
-                        OpCode2(opPointer + 1, opPointer + 2, opPointer + 3, mode1, mode2, mode3);
-                        opPointer += 4;
+                        Multiply(opPointer + 1, opPointer + 2, opPointer + 3, mode1, mode2, mode3);
                         break;
                     case 3:
-                        OpCode3(opPointer + 1, Inputs[inputPointer >= Inputs.Count ? Inputs.Count - 1 : inputPointer]);
-                        inputPointer++;
-                        opPointer += 2;
+                        Input(opPointer + 1, Inputs[inputPointer >= Inputs.Count ? Inputs.Count - 1 : inputPointer]);
                         break;
                     case 4:
-                        output = OpCode4(opPointer + 1, mode1);
-                        opPointer += 2;
+                        output = Output(opPointer + 1, mode1);
                         break;
                     case 5:
-                        OpCode5(++opPointer, ++opPointer, mode1, mode2, ref opPointer);
+                        JumpNZ(++opPointer, ++opPointer, mode1, mode2);
                         break;
                     case 6:
-                        OpCode6(++opPointer, ++opPointer, mode1, mode2, ref opPointer);
+                        JumpZ(++opPointer, ++opPointer, mode1, mode2);
                         break;
                     case 7:
-                        OpCode7(++opPointer, ++opPointer, ++opPointer, mode1, mode2);
-                        opPointer += 1;
+                        LessThan(++opPointer, ++opPointer, ++opPointer, mode1, mode2);
                         break;
                     case 8:
-                        OpCode8(++opPointer, ++opPointer, ++opPointer, mode1, mode2);
-                        opPointer += 1;
+                        Equals(++opPointer, ++opPointer, ++opPointer, mode1, mode2);
                         break;
                 }
             }
@@ -109,69 +101,78 @@ namespace advent_of_code_2019.Day07
             return output;
         }
 
-        private void OpCode1(int p1, int p2, int p3, int i1, int i2, int i3)
+        private void Add(int p1, int p2, int p3, int i1, int i2, int i3)
         {
             var v1 = i1 == 0 ? intCode[intCode[p1]] : intCode[p1];
             var v2 = i2 == 0 ? intCode[intCode[p2]] : intCode[p2];
 
-            //Debug.WriteLine($"ADD {v1} , {v2} INTO {intCode[p3]} : {i1}, {i2}, {i3}");
             intCode[intCode[p3]] = v1 + v2;
+            opPointer += 4;
+
+            //Debug.WriteLine($"ADD {v1} , {v2} INTO {intCode[p3]} : {i1}, {i2}, {i3}");
         }
 
-        private void OpCode2(int p1, int p2, int p3, int i1, int i2, int i3)
+        private void Multiply(int p1, int p2, int p3, int i1, int i2, int i3)
         {
-            //Debug.WriteLine($"MULT {intCode[p1]} , {intCode[p2]} INTO {intCode[p3]}  : {i1}, {i2}, {i3}");
             intCode[intCode[p3]] = (i1 == 0 ? intCode[intCode[p1]] : intCode[p1]) * (i2 == 0 ? intCode[intCode[p2]] : intCode[p2]);
+            opPointer += 4;
+
+            //Debug.WriteLine($"MULT {intCode[p1]} , {intCode[p2]} INTO {intCode[p3]}  : {i1}, {i2}, {i3}");
         }
 
-        private void OpCode3(int p1, int input)
+        private void Input(int p1, int input)
         {
-            //Debug.WriteLine($"WRITE {input} to {intCode[p1]}");
             intCode[intCode[p1]] = input;
+            inputPointer++;
+            opPointer += 2;
+
+            //Debug.WriteLine($"WRITE {input} to {intCode[p1]}");
         }
 
-        private int OpCode4(int p1, int i1)
+        private int Output(int p1, int i1)
         {
             var v1 = i1 == 0 ? intCode[intCode[p1]] : intCode[p1];
             Outputs.Push(v1);
             //Debug.WriteLine($"OUTPUT {v1}");
             IsHalted = true;
+
+            opPointer += 2;
             return v1;
         }
 
-        private void OpCode5(int p1, int p2, int i1, int i2, ref int pointer)
+        private void JumpNZ(int p1, int p2, int i1, int i2)
         {
             var v1 = i1 == 0 ? intCode[intCode[p1]] : intCode[p1];
 
             if (v1 != 0)
             {
-                pointer = i2 == 0 ? intCode[intCode[p2]] : intCode[p2];
+                opPointer = i2 == 0 ? intCode[intCode[p2]] : intCode[p2];
                 //Debug.WriteLine($"JUMPNZ to {pointer}");
             }
             else
             {
                 //Debug.WriteLine("NOP JUMPNZ");
-                pointer++;
+                opPointer++;
             }
         }
 
-        private void OpCode6(int p1, int p2, int i1, int i2, ref int pointer)
+        private void JumpZ(int p1, int p2, int i1, int i2)
         {
             var v1 = i1 == 0 ? intCode[intCode[p1]] : intCode[p1];
 
             if (v1 == 0)
             {
-                pointer = i2 == 0 ? intCode[intCode[p2]] : intCode[p2];
+                opPointer = i2 == 0 ? intCode[intCode[p2]] : intCode[p2];
                 // Debug.WriteLine($"JUMPZ to {pointer}");
             }
             else
             {
                 //Debug.WriteLine("NOP JUMPZ");
-                pointer++;
+                opPointer++;
             }
         }
 
-        private void OpCode7(int p1, int p2, int p3, int i1, int i2)
+        private void LessThan(int p1, int p2, int p3, int i1, int i2)
         {
             var v1 = i1 == 0 ? intCode[intCode[p1]] : intCode[p1];
             var v2 = i2 == 0 ? intCode[intCode[p2]] : intCode[p2];
@@ -184,9 +185,11 @@ namespace advent_of_code_2019.Day07
             {
                 intCode[intCode[p3]] = 0;
             }
+
+            opPointer += 1;
         }
 
-        private void OpCode8(int p1, int p2, int p3, int i1, int i2)
+        private void Equals(int p1, int p2, int p3, int i1, int i2)
         {
             var v1 = i1 == 0 ? intCode[intCode[p1]] : intCode[p1];
             var v2 = i2 == 0 ? intCode[intCode[p2]] : intCode[p2];
@@ -199,8 +202,9 @@ namespace advent_of_code_2019.Day07
             {
                 intCode[intCode[p3]] = 0;
             }
-        }
 
+            opPointer += 1;
+        }
 
         private void GetInput(string input)
         {

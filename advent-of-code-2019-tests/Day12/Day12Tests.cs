786f7f2ce0ebf9d842c53e1cb9058d06f4632fbb
Gareth Spring <gareth.spring@dunnhumby.com>
2019-12-12 10:41:34 +0000

    Day 12 Part 1

---
+++ advent-of-code-2019-tests/Day12/Day12Tests.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace advent_of_code_2019_tests.Day12
+{
+    public class Day12Tests
+    {
+        [Fact]
+        public void Day12Part1()
+        {
+            var day = new advent_of_code_2019.Day12.Day12();
+
+            int actual = day.Part1();
+
+            Assert.True(actual.Equals(7687));
+        }
+
+        [Fact]
+        public void Day12Part2()
+        {
+            var day = new advent_of_code_2019.Day12.Day12();
+
+            long actual = day.Part2();
+
+            Assert.True(actual.Equals(334945516288044));
+        }
+    }
+}

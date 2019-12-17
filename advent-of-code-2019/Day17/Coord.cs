9e3bd3ffc0d90ef77e6da452fbe54aed9aebc0c8
Gareth Spring <gareth.spring@gmail.com>
2019-12-17 21:52:41 +0000

    Day 17 Part 1

---
+++ advent-of-code-2019/Day17/Coord.cs
@@ -0,0 +1,11 @@
+﻿namespace advent_of_code_2019.Day17
+{
+    public class Coord
+    {
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public int Content { get; set; }
+    }
+}
---
+++ advent-of-code-2019/Day17/Day17.cs
@@ -0,0 +1,110 @@
+﻿using advent_of_code_2019.Common;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace advent_of_code_2019.Day17
+{
+    public class Day17
+    {
+        private List<Coord> coords = new List<Coord>();
+
+        private const int Scaffold = 35;
+        private const int NewLine = 10;
+
+        public long Part1(string program, long initialInput)
+        {
+            var CPU = new Cpu(program, new List<long>() { initialInput });
+
+            int x = 0;
+            int y = 1;
+
+            while (!CPU.IsFinished)
+            {
+                var output = CPU.Run();
+
+                if (output == NewLine)
+                {
+                    x = 0;
+                    y++;
+                    continue;
+                }
+                
+                x++;
+
+                var coord = new Coord
+                {
+                    X = x,
+                    Y = y,
+                    Content = (int)output
+                };
+
+                coords.Add(coord);
+            }
+
+            //Render();
+
+            var result = GetAlignmentParameters();
+
+            return result;
+        }
+
+        private void Render()
+        {
+            for (int y = 1; y <= coords.Max(c => c.Y); y++)
+            {
+                for (int x = 1; x <= coords.Max(c => c.X); x++)
+                {
+                    var coord = coords.FirstOrDefault(c => c.X == x && c.Y == y);
+
+                    if (coord == null)
+                    {
+                        Debug.Write("?");
+                    }
+                    else
+                    {
+                        char character = (char)coord.Content;
+                        string text = character.ToString();
+
+                        Debug.Write(text);
+                    }
+                }
+
+                Debug.WriteLine("");
+            }
+           
+        }
+
+        private int GetAlignmentParameters()
+        {
+            var parameters = new List<int>();
+
+            int maxX = 37;
+            int maxY = 43;
+
+            for (int y = 2; y < maxY; y++)
+            {
+                for (int x = 2; x < coords.Max(c => c.X); x++)
+                {
+                    if (
+                        GetCoord(x, y).Content == Scaffold
+                        && GetCoord(x - 1, y).Content == Scaffold
+                        && GetCoord(x + 1, y).Content == Scaffold
+                        && GetCoord(x, y + 1).Content == Scaffold
+                        && GetCoord(x, y - 1).Content == Scaffold
+                        )
+                    {
+                        parameters.Add((x - 1) * (y - 1));
+                    }
+                }
+            }
+
+            return parameters.Sum();
+        }
+
+        private Coord GetCoord(int x, int y)
+        {
+            return coords.FirstOrDefault(c => c.X == x && c.Y == y);
+        }
+    }
+}
